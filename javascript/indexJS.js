449424b6e3228e51763f035ef807b3e2dd379f2f
naseebahm <naseebah1414@gmail.com>
2019-11-06 18:09:02 +0300

    Create indexJS.js

---
+++ javascript/indexJS.js
@@ -0,0 +1,16 @@
+function Roll(){
+var winner = document.querySelector("#winner");
+var die1= document.querySelector("#die1");
+var die2= document.querySelector("#die2");
+
+var randomD1= Math.floor(Math.random()*6)+1;
+var randomD2= Math.floor(Math.random()*6)+1;
+
+die1.setAttribute("src","img/"+randomD1+".png");
+die2.setAttribute("src","img/"+randomD2+".png");
+if(randomD1>randomD2){
+  winner.innerHTML="Player 1 is the Winner!";
+}else if(randomD1<randomD2) {
+  winner.textContent="Player 2 is the Winner!";
+} else {winner.textContent="The Player 1 and The Player 2 are Draw!!";}
+}
